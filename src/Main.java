8d2811a8b947f13eb16f2b1eefd54a8f7238dad3
sonke <inwoker123@mail.ru>
2021-11-07 22:15:49 +0300

    Recording1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000; //  баланс
+        int transfer = 500_000_000; // сумма перевода
+        int total = balance + transfer; // итоговое значение
+        System.out.println(total);
+    }
+}
